2e43afcce3053ee5d63fb43dce60e2e67963e68c
omkardongre <omkardongre5@gmail.com>
2024-09-17 14:49:41 +0530

    chore(frontend): Add Prettier and update ESLint configuration
    -Install Prettier and related dependencies
    -Create .prettierrc and .prettierignore files
    -Update ESLint configuration to work with Prettier
    -Modify package.json scripts for linting and formatting

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,28 +1,52 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import react from "eslint-plugin-react";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ["dist", "node_modules"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      react: react,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
-)
+  eslintConfigPrettier,
+);
