1828f00df9fc86ddaa1914b36cade1bed698b88a
omkardongre <omkardongre5@gmail.com>
2024-10-01 10:37:24 +0530

    Refactor(frontend): Improve UsernameMenu component
    - Update import statements for UI components
    - Remove unnecessary React.FC type annotation
    - Adjust component structure and styling
    - Simplify DropdownMenuItem usage

--- frontend/src/components/UsernameMenu.tsx
+++ frontend/src/components/UsernameMenu.tsx
@@ -1,35 +1,35 @@
-import { useAuth0 } from '@auth0/auth0-react';
+import { CircleUserRound } from 'lucide-react';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from '@radix-ui/react-dropdown-menu';
-import { Separator } from '@radix-ui/react-separator';
-import { CircleUserRound } from 'lucide-react';
+} from './ui/dropdown-menu';
+import { useAuth0 } from '@auth0/auth0-react';
 import { Link } from 'react-router-dom';
+import { Separator } from './ui/separator';
 import { Button } from './ui/button';
 
-const UsernameMenu: React.FC = () => {
+const UsernameMenu = () => {
   const { user, logout } = useAuth0();
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="flex items-center gap-2 px-3 font-bold hover:text-orange-500">
+      <DropdownMenuTrigger className="flex items-center px-3 font-bold hover:text-orange-500 gap-2">
         <CircleUserRound className="text-orange-500" />
         {user?.email}
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        <DropdownMenuItem asChild>
+        <DropdownMenuItem>
           <Link to="/user-profile" className="font-bold hover:text-orange-500">
             User Profile
           </Link>
         </DropdownMenuItem>
         <Separator />
-        <DropdownMenuItem asChild>
+        <DropdownMenuItem>
           <Button
             onClick={() => logout()}
-            className="flex-1 font-bold bg-orange-500"
+            className="flex flex-1 font-bold bg-orange-500"
           >
             Log Out
           </Button>
