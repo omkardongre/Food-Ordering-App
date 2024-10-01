06412823b6145093c476f0d0848cc42301c71ad1
omkardongre <omkardongre5@gmail.com>
2024-10-01 18:10:45 +0530

    feature(frontend): Add cuisines selection to restaurant management form
    - Add cuisine list configuration
    - Add shadcn checkbox component
    - Implement CuisinesSection and CuisineCheckBox components
    - Integrate cuisines selection in ManageRestaurantForm

--- frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -3,6 +3,8 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import DetailSection from './DetailsSection';
+import { Separator } from '@radix-ui/react-separator';
+import CuisinesSection from './CuisinesSection';
 
 const formSchema = z.object({
   restaurantName: z.string({
@@ -61,6 +63,8 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
         className="space-y-8 bg-gray-50 p-10 rounded-lg"
       >
         <DetailSection />
+        <Separator />
+        <CuisinesSection />
       </form>
     </Form>
   );
