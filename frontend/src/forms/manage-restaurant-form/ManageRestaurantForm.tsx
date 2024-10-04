9460c771b3d790573bc19e067f35d4a83f1cf0d4
omkardongre <omkardongre5@gmail.com>
2024-10-04 15:51:53 +0530

    feat(frontend): Implement Create Restaurant API integration
    - Add Restaurant and MenuItem types
    - Prepare form data for API submission
    - Integrate useCreateMyRestaurant hook in ManageRestaurantPage
    - Update ManageRestaurantForm to handle form submission

--- frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -9,6 +9,7 @@ import MenuSection from './MenuSection';
 import ImageSection from './ImageSection';
 import LoadingPageButton from '@/components/LoadingButton';
 import { Button } from '@/components/ui/button';
+import { Restaurant } from '@/types';
 
 const formSchema = z.object({
   restaurantName: z.string({
@@ -24,6 +25,12 @@ const formSchema = z.object({
     required_error: 'Delivery price is required',
     invalid_type_error: 'Delivery price must be a number',
   }),
+
+  estimatedDeliveryTime: z.coerce.number({
+    required_error: 'Estimated delivery time is required',
+    invalid_type_error: 'Estimated delivery time must be a number',
+  }),
+
   cuisines: z.array(z.string()).nonempty({
     message: 'Cuisines are required',
   }),
@@ -42,23 +49,44 @@ const formSchema = z.object({
   imageFile: z.instanceof(File, { message: 'Image is required' }),
 });
 
-type restaurantFormData = z.infer<typeof formSchema>;
+type RestaurantFormData = z.infer<typeof formSchema>;
 
 type Props = {
-  onSave: (restaurantFormData: FormData) => void;
+  onSave: (restaurantFormData: FormData) => Promise<Restaurant>;
   isLoading: boolean;
 };
 
-const onSubmit = (formDataJson: restaurantFormData) => {};
-
 const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
-  const form = useForm<restaurantFormData>({
+  const form = useForm<RestaurantFormData>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       cuisines: [],
       menuItems: [{ name: '', price: 1 }],
     },
   });
+
+  const onSubmit = (formDataJson: RestaurantFormData) => {
+    const formData = new FormData();
+
+    formData.append('restaurantName', formDataJson.restaurantName);
+    formData.append('city', formDataJson.city);
+    formData.append('country', formDataJson.country);
+    formData.append('deliveryPrice', formDataJson.deliveryPrice.toString());
+    formData.append(
+      'estimatedDeliveryTime',
+      formDataJson.estimatedDeliveryTime.toString()
+    );
+    formDataJson.cuisines.forEach((cuisine, index) => {
+      formData.append(`cuisines[${index}]`, cuisine);
+    });
+    formDataJson.menuItems.forEach((menuItem, index) => {
+      formData.append(`menuItems[${index}][name]`, menuItem.name);
+      formData.append(`menuItems[${index}][price]`, menuItem.price.toString());
+    });
+    formData.append('imageFile', formDataJson.imageFile);
+
+    onSave(formData);
+  };
 
   return (
     <Form {...form}>
