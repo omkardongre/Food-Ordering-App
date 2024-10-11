1e677f7d717033ef940bb273c9dc81aa28b1bc2e
omkardongre <omkardongre5@gmail.com>
2024-10-11 13:23:57 +0530

    feature(frontend): Implement restaurant search by name and cuisine
    - Update RestaurantApi to include search query in API requests
    - Enhance SearchBar component to handle initial search query and resets
    - Implement search functionality in SearchPage component
    - Add SearchState type to manage search query state
    - Update SearchResultInfo to display total results and city

--- frontend/src/api/RestaurantApi.tsx
+++ frontend/src/api/RestaurantApi.tsx
@@ -1,17 +1,23 @@
+import { SearchState } from '@/pages/SearchPage';
 import { RestaurantResponse } from '@/types';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useQuery } from 'react-query';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const useSearchRestaurants = (city?: string) => {
+export const useSearchRestaurants = (
+  searchState: SearchState,
+  city: string
+) => {
   const { getAccessTokenSilently } = useAuth0();
 
   const createSearchRestaurant = async (): Promise<RestaurantResponse> => {
     const accessToken = await getAccessTokenSilently();
+    const params = new URLSearchParams();
+    params.set('searchQuery', searchState.searchQuery);
 
     const response = await fetch(
-      `${API_BASE_URL}/api/restaurant/search/${city}`,
+      `${API_BASE_URL}/api/restaurant/search/${encodeURIComponent(city)}?${params.toString()}`,
       {
         method: 'GET',
         headers: {
@@ -28,7 +34,7 @@ export const useSearchRestaurants = (city?: string) => {
   };
 
   const { data: results, isLoading } = useQuery(
-    ['searchRestaurants'],
+    ['searchRestaurants', searchState],
     createSearchRestaurant,
     { enabled: !!city }
   );
--- frontend/src/components/SearchResultInfo.tsx
+++ frontend/src/components/SearchResultInfo.tsx
@@ -2,19 +2,19 @@ import { Link } from 'react-router-dom';
 
 type Props = {
   total: number;
-  city?: string;
+  city: string;
 };
 
 const SearchResultInfo = ({ total, city }: Props) => {
   return (
-    <div className="text-xl font-bold flex flex-col gap-3 justify-between lg:items-center lg:flex-row lg-flex-row">
+    <div className="text-xl font-bold flex flex-col gap-3 justify-between lg:items-center lg:flex-row">
       <span>
-        {total} Restaurant found in {city}
+        {total} {total === 1 ? 'Restaurant' : 'Restaurants'} found in {city}
         <Link
           to="/"
           className="ml-1 text-sm font-semibold underline cursor-pointer text-blue-500"
         >
-          Change location
+          Change Location
         </Link>
       </span>
       Insert sort dropdown here :)
