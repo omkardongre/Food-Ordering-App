d6dfb72475fa87c44ef02ca746f8572c139ae813
omkardongre <omkardongre5@gmail.com>
2024-10-15 12:44:28 +0530

    feature(frontend): Implement add to cart functionality
    - Add addToCart function in DetailPage to manage cart state
    - Update FoodItemMenu component to trigger addToCart on click
    - Create OrderSummary component to display cart items and total
    - Implement CartItem type for managing items in the cart
    - Add Badge component from shadcn UI library for visual enhancements
    - Update DetailPage layout to include OrderSummary alongside menu items

--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -1,12 +1,60 @@
 import { useGetRestaurant } from '@/api/RestaurantApi';
 import FoodItemMenu from '@/components/FoodItemMenu';
+import OrderSummary from '@/components/OrderSummary';
 import RestaurantInfo from '@/components/RestaurantInfo';
+import { Card } from '@/components/ui/card';
+import { MenuItem } from '@/types';
 import { AspectRatio } from '@radix-ui/react-aspect-ratio';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+
+export type CartItem = {
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
 
 const DetailPage = () => {
   const { restaurantId } = useParams<{ restaurantId: string }>();
   const { restaurant, isLoading } = useGetRestaurant(restaurantId as string);
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const addToCart = (menuItem: MenuItem) => {
+    setCartItems((prevCartItems) => {
+      //if cart is not present add to cartItem
+      // if cart is present update quantity
+
+      const existingItem = prevCartItems.find(
+        (item) => item._id === menuItem._id
+      );
+
+      let newCartItems = [];
+      if (existingItem) {
+        newCartItems = prevCartItems.map((item) => {
+          if (item._id === menuItem._id) {
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+            };
+          }
+          return item;
+        });
+      } else {
+        newCartItems = [
+          ...prevCartItems,
+          {
+            _id: menuItem._id,
+            name: menuItem.name,
+            price: menuItem.price,
+            quantity: 1,
+          },
+        ];
+      }
+
+      return newCartItems;
+    });
+  };
 
   if (isLoading || !restaurant) {
     return <div>Loading...</div>;
@@ -26,8 +74,16 @@ const DetailPage = () => {
           <RestaurantInfo restaurant={restaurant} />
           <span className="text-2xl font-bold tracking-tight">Menu</span>
           {restaurant.menuItems.map((menuItem) => (
-            <FoodItemMenu menuItem={menuItem} />
+            <FoodItemMenu
+              menuItem={menuItem}
+              addToCart={() => addToCart(menuItem)}
+            />
           ))}
+        </div>
+        <div>
+          <Card>
+            <OrderSummary restaurant={restaurant} cartItems={cartItems} />
+          </Card>
         </div>
       </div>
     </div>
